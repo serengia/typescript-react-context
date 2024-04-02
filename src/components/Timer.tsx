2408e6f3e11b6e76f99eaa41c65496975470d11f
James Serengia <jamesserengia@gmail.com>
2024-04-02 14:18:29 +0300

    Implement running timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "./context/TimersContext.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prev) => prev - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
