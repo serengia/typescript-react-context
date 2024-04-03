1e72698cc9e0107a1599ecdaa56dcfb50beb8df0
James Serengia <jamesserengia@gmail.com>
2024-04-03 09:51:32 +0300

    Add clear timers functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "./context/TimersContext.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "./context/TimersContext.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const timerRef = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
 
+  if (remainingTime <= 0 && timerRef.current) {
+    clearInterval(timerRef.current);
+  }
+
+  let timer: number;
   useEffect(() => {
-    setInterval(() => {
-      setRemainingTime((prev) => prev - 50);
-    }, 50);
-  }, []);
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prev) => prev - 50);
+      }, 50);
+
+      timerRef.current = timer;
+    } else if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
